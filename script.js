a38fb13a74ceea8b0589ef84b9831cdddf3828bd
Abhiank Awasthi <abhiankawasthi@gmail.com>
2026-08-12 18:01:48 +0530

    Made the NxN squares generation completely dynamic in nature

--- script.js
+++ script.js
@@ -7,56 +7,71 @@ let parentElement;
 button.addEventListener("pointerdown" , function(e) {
     event.preventDefault();
     const value = Number(prompt("How many squares per row do you want to display? \n (1 - 10)"));
+
     if(value > 10 || value < 1) {
         alert("please enter the numbers in a valid range! (1 - 10)");
     }
+
     else if(Number.isNaN(value)) {
         alert("Please enter a valid number.");
     }
-});
 
-for(let i=1;i<17;i++){
-    if(i == (16**0.5)*j + 1 && Math.floor(j) == j) {
-        parentElement = document.createElement('div');
-        const childElement = document.createElement('div');
-
-        parentElement.setAttribute('class','parent');
-        childElement.setAttribute('class','child');
-
-        container.appendChild(parentElement);
-        parentElement.appendChild(childElement);
-
-        childElement.addEventListener("mouseover" , function(event) {
-            event.target.style.backgroundColor = 
-            `rgb(
-            ${Math.floor(Math.random() * 256)},
-            ${Math.floor(Math.random() * 256)},
-            ${Math.floor(Math.random() * 256)}
-            )`;
-        });
-        childElement.addEventListener("mouseout" , function(event) {
-            event.target.style.backgroundColor = `rgb(255 , 255 , 255)`;
-        });
-
-        j++;
-    }
     else {
-        const childElements = document.createElement('div');
+        j = 0;
+        const valueSquared = value**2;
+        container.innerHTML = "";
+        for(let i=1; i<valueSquared + 1; i++){
+            if(i == (valueSquared**0.5)*j + 1) {
+                parentElement = document.createElement('div');
+                const childElement = document.createElement('div');
 
-        childElements.setAttribute('class','child');
+                parentElement.setAttribute('class','parent');
+                childElement.setAttribute('class','child');
 
-        parentElement.appendChild(childElements);
+                container.appendChild(parentElement);
+                parentElement.appendChild(childElement);
 
-        childElements.addEventListener("mouseover" , function(event) {
-            event.target.style.backgroundColor = 
-            `rgb(
-            ${Math.floor(Math.random() * 256)},
-            ${Math.floor(Math.random() * 256)},
-            ${Math.floor(Math.random() * 256)}
-            )`;
-        });
-        childElements.addEventListener("mouseout" , function(event) {
-            event.target.style.backgroundColor = `rgb(255, 255, 255)`;
-        });
+                if (value > 6) {
+                    childElement.classList.add('reduced-padding');
+                }
+
+                childElement.addEventListener("mouseover" , function(event) {
+                    event.target.style.backgroundColor = 
+                    `rgb(
+                    ${Math.floor(Math.random() * 256)},
+                    ${Math.floor(Math.random() * 256)},
+                    ${Math.floor(Math.random() * 256)}
+                    )`;
+                });
+                childElement.addEventListener("mouseout" , function(event) {
+                    event.target.style.backgroundColor = `rgb(255 , 255 , 255)`;
+                });
+
+                j++;
+            }
+            else {
+                const childElements = document.createElement('div');
+
+                childElements.setAttribute('class','child');
+
+                parentElement.appendChild(childElements);
+
+                if (value > 6) {
+                    childElements.classList.add('reduced-padding');
+                }
+
+                childElements.addEventListener("mouseover" , function(event) {
+                    event.target.style.backgroundColor = 
+                    `rgb(
+                    ${Math.floor(Math.random() * 256)},
+                    ${Math.floor(Math.random() * 256)},
+                    ${Math.floor(Math.random() * 256)}
+                    )`;
+                });
+                childElements.addEventListener("mouseout" , function(event) {
+                    event.target.style.backgroundColor = `rgb(255, 255, 255)`;
+                });
+            }
+        }
     }
-}
+});
