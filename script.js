9fda6d794d3388099121b9977fc0350e6f14508a
Abhiank Awasthi <abhiankawasthi@gmail.com>
2026-08-12 17:29:23 +0530

    Made the control-flow statements and if prompt's value isn't b/w 0-10 or is NAN it returns these statements

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+
 let j = 0;
 let parentElement;
+
+button.addEventListener("pointerdown" , function(e) {
+    event.preventDefault();
+    const value = Number(prompt("How many squares per row do you want to display? \n (1 - 10)"));
+    if(value > 10 || value < 1) {
+        alert("please enter the numbers in a valid range! (1 - 10)");
+    }
+    else if(Number.isNaN(value)) {
+        alert("Please enter a valid number.");
+    }
+});
 
 for(let i=1;i<17;i++){
     if(i == (16**0.5)*j + 1 && Math.floor(j) == j) {
